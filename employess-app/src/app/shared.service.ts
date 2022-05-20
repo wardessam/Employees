f00a75fc3bea9e76fda8e81e7d10c36d0937e286
Wardshan Essam <rose.essam38@gmail.com>
2022-05-20 15:33:15 +0200

    Added Routing and api methods

--- employess-app/src/app/shared.service.ts
+++ employess-app/src/app/shared.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
-
-  constructor() { }
+readonly API_URL ="https://localhost:7129/api";
+readonly Photo_URL = "https://localhost:7129/Photos";
+  constructor(private http: HttpClient) { }
+  //Department Methods
+  getDepartments():Observable<any[]>{
+    return this.http.get<any>(this.API_URL+'/department');
+  }
+  addDepartment(val:any){
+    return this.http.post(this.API_URL+'/Department',val)
+  }
+  updateDepartment(val:any){
+    return this.http.put(this.API_URL+'/Department',val)
+  }
+  deleteDepartment(val:any){
+    return this.http.delete(this.API_URL+'/Department/',val)
+  }
+  //Employee Methods
+  getEmployees():Observable<any[]>{
+    return this.http.get<any>(this.API_URL+'/Employee');
+  }
+  addEmployee(val:any){
+    return this.http.post(this.API_URL+'/Employee',val)
+  }
+  updateEmployee(val:any){
+    return this.http.put(this.API_URL+'/Employee',val)
+  }
+  deleteEmployee(val:any){
+    return this.http.delete(this.API_URL+'/Employee/',val)
+  }
+  //Uploading Profile Picture
+  uploadPhoto(val:any){
+    return this.http.post(this.API_URL+'/Employee/SaveFile',val);
+  }
+  //Getting all departments names
+  getDepsNames():Observable<any[]>{
+    return this.http.get<any[]>(this.API_URL+'/Employee/GetAllDepartmentsNames');
+  }
+  //Getting Family Members
+  getFamilyMembers(val:any){
+    return this.http.get(this.API_URL+'/Employee/GetFamilyMembers/',val);
+  }
+  //Adding Family Member
+  addFamilyMember(val:any){
+    return this.http.post(this.API_URL+'/Employee/AddFamilyMember',val);
+  }
+  //Deleting Family Member
+  deleteFamilyMember(val:any){
+    return this.http.delete(this.API_URL+'/Employee/DeleteFamilyMember/',val);
+  }
 }
